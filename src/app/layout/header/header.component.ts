c3b7267090339b59dddba2d56a09836311ead315
nenadmne <cosovicnenad14@gmail.com>
2024-07-09 19:25:13 +0200

    Added 0.5 second debounce delay on search input

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
 import { ProductsService } from '../../services/products.service';
 import { Product, Products } from '../../../Types';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { debounceTime, distinctUntilChanged } from 'rxjs';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
@@ -13,16 +15,32 @@ export class HeaderComponent {
   constructor(private productService: ProductsService) {}
 
   products: Product[] = [];
+  keyword: string = '';
+
+  searchControl = new FormControl();
 
   fetchProducts(q: string) {
     this.productService
       .searchProducts('https://dummyjson.com/products/search', { q })
       .subscribe((products: Products) => {
-        console.log(products);
+        console.log(products.products);
+        this.products = products.products;
       });
   }
 
+  // Function for filtering products on every keystroke with debounce delay of 0.5 second
   ngOnInit() {
-    this.fetchProducts('phone');
+    this.searchControl.valueChanges
+      .pipe(debounceTime(500), distinctUntilChanged())
+      .subscribe((value) => {
+        if (value) {
+          this.fetchProducts(value);
+        }
+      });
+  }
+
+  // Preventing default behaviour of form element
+  onSearch(event: Event) {
+    event.preventDefault();
   }
 }
